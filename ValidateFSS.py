b329d03857dc7b0a8ff79a887f8a09fb85c45503
Loinen <dance_cucumber@mail.ru>
2020-11-27 17:13:15 +0300

    try niapy

--- ValidateFSS.py
+++ ValidateFSS.py
@@ -3,6 +3,12 @@ from ObjectiveFunction import *
 from SearchSpaceInitializer import UniformSSInitializer, OneQuarterDimWiseSSInitializer
 import numpy as np
 import os
+import NiaPy as nia
+from NiaPy.algorithms.basic import GreyWolfOptimizer
+from NiaPy.task import StoppingTask, OptimizationType
+from NiaPy.benchmarks import Sphere
+from NiaPy.benchmarks import rosenbrock
+
 
 def create_dir(path):
     directory = os.path.dirname(path)
@@ -72,6 +78,32 @@ def run_experiments(n_iter, school_size, num_runs, objective_function, search_sp
         f_handle_cost_eval.close()
 
 
+def rosen(x):
+    """The Rosenbrock function"""
+    return np.sum(100.0*(x[1:]-x[:-1]**2.0)**2.0 + (1-x[:-1])**2.0, axis=0)
+
+
 if __name__ == '__main__':
-    print ("starting FSS")
-    main()
+    x = np.random.normal(size=1000)
+    x1 = np.random.normal(size=1000)
+    fss = nia.algorithms.basic.FishSchoolSearch(Name=['FSS', 'FishSchoolSearch'], school=100)
+    fss.gen_weight()
+    fss.setParameters(NP=100, SI_init=0.3, SI_final=1, SV_init=0.3, SV_final=1, min_w=0.3, w_scale=0.7)
+    # fss.runIteration(nia.benchmarks.rosenbrock, x, x, x1, 1.5, 1, 2, 3, 4)
+    gw = nia.algorithms.basic.GreyWolfOptimizer(Name=['GreyWolfOptimizer', 'GWO'], pop=10, task=rosen)
+    print(fss.getBest(X=x, X_f=x1), fss.bad_run())
+    print(fss.getParameters())
+    print(gw.getBest(X=x, X_f=x1), gw.bad_run())
+    print(gw.getParameters())
+    print("starting FSS")
+
+    # we will run Grey Wolf Optimizer for 5 independent runs
+    for i in range(5):
+        task = StoppingTask(D=5, nFES=1000, optType=OptimizationType.MINIMIZATION, benchmark=Sphere())
+        algo = GreyWolfOptimizer(NP=40)
+        best = algo.run(task)
+        print(best)
+    # main()
+
+
+
