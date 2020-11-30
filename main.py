ddee3b0240fa98b14c6851e1913e3c645a61e527
Loinen <dance_cucumber@mail.ru>
2020-11-30 18:52:55 +0300

    clean up and 3d plot posen

--- main.py
+++ main.py
@@ -39,20 +39,22 @@ if __name__ == '__main__':
     #     print(best[-1])
     nruns = 5
     stats = np.zeros(nruns)
+    benc = Rosenbrock()
+    benc.plot3d()
     for i in range(nruns):
         task = StoppingTask(D=2, nGEN=100, nFES=1000, optType=OptimizationType.MINIMIZATION, benchmark=Rosenbrock())
         algo = FishSchoolSearch(NP=40, SI_init=0.3, SI_final=1, SV_init=0.3, SV_final=1, min_w=0.2, w_scale=0.8)
-        # plt.plot(Rosenbrock())
         best = algo.run(task)
         # print("FSS", best[-1])
-        # task.plot()
+        task.plot()
         stats[i] = best[1]  # save best
         evals, x_f = task.return_conv()
-        print(evals)  # print function evaluations
-        print(x_f)  # print values
+        # print(evals)  # print function evaluations
+        # print(x_f)  # print values
 
     stat = BasicStatistics(stats)
     print(stat.generate_standard_report())  # generate report
+    print(algo.getParameters())
 
     # main()
 
